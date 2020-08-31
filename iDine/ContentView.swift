a68774bf680f69165ba3ab4f08bef92736d4e2a0
Marcel <mchaucer324@gmail.com>
2020-08-31 16:19:44 -0400

    Initial Commit

---
+++ iDine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Marcel on 8/31/20.
+//  Copyright © 2020 Marcel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
